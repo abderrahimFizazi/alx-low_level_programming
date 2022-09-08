a1db9235ab15e8a870ede6fb77a6cec72ad2003d
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-08 07:16:38 -0700

    low language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int intType;
 float floatType;
 long long int doubleType;
 char charType;
-long l;
+long int  li;
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(li));
