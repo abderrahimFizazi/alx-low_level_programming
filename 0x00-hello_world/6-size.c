62e9cbe5685896dfc307588377e6cdd2891c4eef
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-08 07:14:12 -0700

    low language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ long long int doubleType;
 char charType;
 long l;
 long int li;
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long: %zu byte\n", sizeof(l));
-printf("Size of long int: %zu byte\n", sizeof(li));
-printf("Size of long long int: %zu bytes\n", sizeof(doubleType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu bytes(s)\n", sizeof(intType));
+printf("Size of long: %zu byte(s)\n", sizeof(l));
+printf("Size of long int: %zu byte(s)\n", sizeof(li));
+printf("Size of long long int: %zu byte(s)\n", sizeof(doubleType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
