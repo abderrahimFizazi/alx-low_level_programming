5410d0678bdc185333ab9f7ef1f241b85f26e169
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-09 10:07:04 -0700

    if else while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	x = 'z';
+	while (x >= 'a')
+	{
+	putchar("%d", x);
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
+
