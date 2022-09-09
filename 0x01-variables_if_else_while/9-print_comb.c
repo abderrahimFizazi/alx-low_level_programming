fde26ecfde410620f0a136066ab1fbff81e56535
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-09 10:18:42 -0700

    if else while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	x = '0';
+	while (x <= '9')
+	{
+	putchar(x);
+	putchar(',');
+	putchar(' ');
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
+
