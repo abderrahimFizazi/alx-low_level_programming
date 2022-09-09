7e110f2753a716d43fbcef3d7b0693aff21db3c8
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-09 09:45:41 -0700

    if else while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{
+	putchar(x);
+	x++;
+	}
+	x = 'A';
+	while (x <= 'Z')
+	{
+	putchar(x);
+	x++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
