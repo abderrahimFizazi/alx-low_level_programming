a2a9a9907589f294558255c26eab200b95b1e8fd
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-13 07:51:17 -0700

    hey

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - - prints _putchar, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char x = 'a';
+while (x <= 'z')
+{
+_putchar(x);
+x++;
+}
+putchar('\n');
+
+	return (0);
+}
