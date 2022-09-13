ba80819e56a9d6ac921da73b51190bc20dbef0f1
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-13 08:29:28 -0700

    hey

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - - prints _putchar, followed by a new line
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+int _isalpha(int c)
+{
+if (isalpha(c))
+return (1);
+return (0);
+}
