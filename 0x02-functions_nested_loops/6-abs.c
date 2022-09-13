cf266a27ebb7197fd493d2a0a4e6d6c5aa45c704
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-13 08:50:13 -0700

    hey

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - - prints _putchar, followed by a new line
+ * @n : character to check the case
+ * Return:0 or 1 or -1
+ */
+int _abs(int n)
+{
+return (abs(n));
+}
