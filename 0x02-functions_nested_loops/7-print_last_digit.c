7ce19005f8ad3a547801b5d37f8ca7dde06bbd5c
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-13 08:55:45 -0700

    hey

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_last_digit - - prints _putchar, followed by a new line
+ * @n : character to check the case
+ * Return:0 or 1 or -1
+ */
+int print_last_digit(int n)
+{
+return (n%10);
+}
