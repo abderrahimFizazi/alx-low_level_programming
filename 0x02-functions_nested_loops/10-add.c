d8dc4febabc963cdc81cf22279a7fc82c31e9ffe
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-14 11:33:55 -0700

    hey

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add - - prints _putchar, followed by a new line
+ * @n : character to check the case
+ * @y : character to check the case
+ * Return:0 or 1 or -1
+ */
+int add(int n, int y)
+{
+return (n + y);
+}
