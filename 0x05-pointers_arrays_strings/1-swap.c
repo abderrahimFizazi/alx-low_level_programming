959df2cd891e0b478912147e728c798a1181f70e
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-20 01:21:27 -0700

    poiters

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - reset argument to 98
+ * @a: pointer
+ * @b: poiner 3awt
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+
+{
+int *c;
+
+*c = (*a);
+*a = (*b);
+*b = (*c);
+}
