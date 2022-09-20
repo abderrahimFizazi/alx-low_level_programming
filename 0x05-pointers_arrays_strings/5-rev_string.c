9cb9ec7d636b16e0b42a7b011571ae3097f9c8e6
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-20 03:15:08 -0700

    poiters

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * rev_string - reset argument to 98
+ * @s: pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+char *p, *r, temp;
+int i;
+int l;
+
+l = 0;
+for (p = s; *p != '\0'; p++)
+{
+}
+p--;
+for (p, r = s ; r != p; p--, r++)
+{
+temp = *p;
+*p = *s;
+*s = temp;
+}
+printf("\n");
+}
