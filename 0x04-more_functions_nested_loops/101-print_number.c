d3bda43a62321beb3082c1391bdf0528e0517857
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-21 07:13:32 -0700

    4 low

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_number - prints an integer
- *   * @n: tracked var
- *    */
+ * print_number - prints an integer
+ * @n: tracked va
+ */
 
 void print_number(int n)
 {
