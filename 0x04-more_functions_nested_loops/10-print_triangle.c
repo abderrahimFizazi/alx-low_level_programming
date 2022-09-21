e55cf5bf987665cc3482a4c18d6445f0f7e73105
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-21 06:37:02 -0700

    4 low

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,11 +11,11 @@ void print_triangle(int size)
 	{
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= size - i; j++)
+		for (j = 0; j < size - i; j++)
 		{
 			_putchar(' ');
 		}
-		for (j = size - i; j <= size; j++)
+		for (; j < i; j++)
 		{
 			_putchar('#');
 		}
