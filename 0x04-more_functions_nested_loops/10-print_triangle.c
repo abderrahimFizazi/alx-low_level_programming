9a01c874edad343d73fd8e2ff1401674c0e04cfd
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-21 06:49:09 -0700

    4 low

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,32 @@
 /**
- * print_triangle - check for a digit
- * @size : integer type
- * Return:voi
- */
+* print_triangle - check for a digit
+* @size : integer type
+* Return:void
+*/
 
 void print_triangle(int size)
 {
 
-	int i = 1, ii;
+int i = 1, ii;
 
-	while (i <= size && size > 0)
-	{
-		ii = 0;
-		while (ii < size - i)
-		{
-			_putchar(' ');
-			ii++;
-		}
-		ii = 0;
-		while (ii < i)
-		{
-			_putchar('#');
-			ii++;
-		}
+while (i <= size && size > 0)
+{
+ii = 0;
+while (ii < size - i)
+{
+_putchar(' ');
+ii++;
+}
+ii = 0;
+while (ii < i)
+{
+_putchar('#');
+ii++;
+}
 
-		_putchar('\n');
-		i++;
-	}
-	if (i == 1)
-		_putchar('\n');
+_putchar('\n');
+i++;
+}
+if (i == 1)
+_putchar('\n');
 }
