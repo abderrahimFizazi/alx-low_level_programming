b5a3d3289ac390c0fbac45f80f908da1dcd6144e
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-21 05:54:10 -0700

    4 low

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_most_numbers - check the code.
+*/
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 0; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			prinft("Buzz");
+		}
+		else
+		{
+			_putchar('0' + i);
+		}
+		if ( i < 100)
+		{
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
