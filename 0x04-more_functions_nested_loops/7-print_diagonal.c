285123bfbc83baae72e18d563df03b5de237a958
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-21 05:22:28 -0700

    4 low

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - check the code.
+ * @n: hey
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+	if (n > 0)
+	{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		_putchar(' ');
+		_putchar('/');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
