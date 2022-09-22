b119beccf0175f9f87af303f687e3a68836874f5
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-22 10:08:10 -0700

    low 6

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+/**
+ *_strncat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n : nmr
+ * Return: void
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = -1, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+	do {
+	a++;
+	dest[i] = src[a];
+	i++;
+	} while (src[a] != '\0' && a < n);
+
+	return (dest);
+}
