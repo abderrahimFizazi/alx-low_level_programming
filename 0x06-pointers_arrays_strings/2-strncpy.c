5dda39bcf6a48994c9e25eee1f89033e04e95d35
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-22 11:54:37 -0700

    low 6

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 	;
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n ; i++)
 	dest[i] = src[i];
 	dest[dest_len + i] = '\0';
 	return (dest);
