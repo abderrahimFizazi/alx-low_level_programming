4e0fe811d2d0f657e641205931f36df63f30b319
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-22 11:06:00 -0700

    low 6

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,24 @@
 /**
- *_strncat - concatenates two strings
+ * trncat - concatenates n bytes of two strings
  * @dest: pointer destination
  * @src: pointer source
- * @n : nmr
+ * @n: number of bytes
  * Return: void
+ 
  */
 
-char *_strncat(char *dest, char *src, int n)
+ char *_strncat(char *dest, char *src, int n)
 {
-	int a = -1, i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	;
-	do {
-	a++;
-	dest[i] = src[a];
-	i++;
-	} while (src[a] != '\0' && a < n - 1);
+	int dest_len, i;
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+		;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+			dest[dest_len + i] = src[i];
+
+	/*should end with a end of string char*/
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }
