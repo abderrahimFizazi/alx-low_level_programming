91984ba2e81d371a91704cdaef8a429106d9b40d
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-22 12:02:20 -0700

    low 6

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,19 @@
-/**
- * _strncpy - concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @n: number of bytes
- * Return: vo
+/** strncpy - find the length of a string
+ * @dest: pointer to the string
+ * @src: pointer
+ * @n: int
+ * Return: destination
  */
 
-char *_strncpy(char *dest, char *src, int n)
+ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n ; i++)
-	dest[i] = src[i];
-	for (; i < n; i++)
-		dest[i] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+			dest[i] = src[i];
+
+	for ( ; i < n; i++)
+			dest[i] = '\0';
+
 	return (dest);
 }
