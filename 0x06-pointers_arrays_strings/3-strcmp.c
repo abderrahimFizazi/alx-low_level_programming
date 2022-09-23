3d43a179c5915560bf71c0ab86b360ded6a72cce
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-23 03:46:40 -0700

    low 6

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,24 @@
 #include "main.h"
 /**
- * _strncpy - find the length of a string
- * @dest: pointer to the string
- * @src: pointer
- * @n: int
+ * _strcmp - find the length of a string
+ * @s1: pointer to the string
+ * @s2: pointer
  * Return: destination
  */
-
-char *_strncpy(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+	if(s1[i] == s2[i])
+	i++;
+	else
+	{
+		return (s1[i] - s2[i]);
+	}
 
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
-	return (dest);
+}
+return (0);
 }
