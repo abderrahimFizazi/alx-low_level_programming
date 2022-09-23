2f08041a796543ca9fa120f462d4b199f73ddc7a
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-23 04:34:29 -0700

    low 6

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,8 +8,16 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, j, temp;
+	int i, j, temp, half;
 
+	if (n % 2 == 0)
+	{
+		half = n / 2;
+	}
+	else
+	{
+		half = (n - 1) / 2;
+	}
 	for (i = 0, j = n - 1; i != j; i++, j--)
 	{
 		temp = a[i];
