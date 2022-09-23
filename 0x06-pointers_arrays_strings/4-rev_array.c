d478c296027935f83f52d31a15a8dd4ab23e35e2
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-23 04:03:18 -0700

    low 6

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * _strcmp - find the length of a string
+ * reverse_array - find the length of a string
  * @a: pointer to the string
  * @n: pointer
  * Return: destination
  */
+
 void reverse_array(int *a, int n)
 {
 	int i, j, temp;
+
 	for (i = 0, j = n - 1; i != j; i++, j--)
 	{
 		temp = a[i];
