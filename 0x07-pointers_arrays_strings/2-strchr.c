7157c3f0878d6a8f9bd5070f15394edc159bb871
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-27 08:34:45 -0700

    pointers 7

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - prints buffer in hexa
+ * @s: the address of memory to print
+ * @c: the size of the memory to print
+ * Return: Nothing.
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	i = 0;
+	while ( *(s + i) != '\0')
+	{
+		if ( *(s + i) == c)
+		{
+			s = s + i;
+			break;
+		}
+	}
+	return (s);
+}
