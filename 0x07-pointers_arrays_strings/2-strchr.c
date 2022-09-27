1fb2d3029cccad80bb50d5ade451a00a1bd71dff
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-27 08:46:12 -0700

    pointers 7

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,22 @@
 char *_strchr(char *s, char c)
 {
 	
-	while (*s != '\0')
+	while (*s != '\0') /*Declaring WHILE*/
 	{
-		if (*s == c)
-			return (s);
-	s++;
+		if (*s == c) /*if s ==  c*/
+		{
+			return (s); /*return s*/
+		}
+
+		++s;
 	}
 
-	return (0);
+	if (*s == c)
+	{
+		return (s);
+
+	}
+
+
+	return (0);/* values null*/
 }
