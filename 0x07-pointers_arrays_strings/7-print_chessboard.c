edce590b6f3659a6304a9a615d312c00fe3716da
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-09-27 09:09:19 -0700

    pointers 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,26 +1,22 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_chessboard - print the chessboard, 2d array
- * @a: 2d array of chars
- *Return: void
+ * *print_chessboard - description
+ * @a: board
+ * Return: void
  */
+
+
 void print_chessboard(char (*a)[8])
 {
-	/*Declaring variables*/
-	int i, j;
+int i, j;
 
-	i = 0;
-	while (i < 8) /*number repetitions*/
+for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
 	{
-		j = 0;
-		while (j < 8)
-		{
-			_putchar (a[i][j]);
-			j++;
-		}
-		_putchar ('\n'); /*print new line*/
-		i++; /*add +1*/
+		_putchar(a[i][j]);
 	}
+	_putchar('\n');
+}
 
 }
