7ac09e76fbbb2fc64fc48adae9d1b4714fbf69e8
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-10-08 07:47:29 -0700

    weekend

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - check the code
+ * Return: int
+ * @n: var
+ */
+ int factorial(int n)
+{
+	if (n > 1)
+	{
+		return (n * _strlen_recursion(--n));
+	}
+	else if (n == 1 || n == 0)
+	{
+	return (1);	
+	}
+	else
+	{
+	return(-1);
+	}
+}
