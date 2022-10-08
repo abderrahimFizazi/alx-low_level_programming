04e9f421fa47ec8f0a9f821cdb8f5d949a404e6b
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-10-08 07:57:22 -0700

    weekend

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,22 +1,14 @@
 #include "main.h"
 /**
- * _print_rev_recursion - check the code
- *
- * @s: var
- */
+* _print_rev_recursion - function that prints a string in reverse.
+*@s: parameter string
+*
+*/
 void _print_rev_recursion(char *s)
 {
-	char *i;
 	if (*s != '\0')
 	{
-	for (i = s; *i != '\0'; i++);
-	i--;
-	s++;
-	_putchar(*i);
-	_print_rev_recursion(s);
-	}
-	else
-	{
-		_putchar('\n');
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
