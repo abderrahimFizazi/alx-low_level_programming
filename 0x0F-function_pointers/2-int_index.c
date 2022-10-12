80141898ce10caab09fc909e13be1daea4f4b189
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-10-12 07:07:49 -0700

    hey

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,21 +2,23 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter
+ * int_index - executes a function given as a parameter
  * on each element of an array
  * @array: array to iterate over
  * @size: size of the array
  * @cmp: pointer to function used
+ * Return: test
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	unsigned int i;
+
 	if (!cmp || !array || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if(cmp(array[i]) 
+		if (cmp(array[i])
 			return (i);
 	}
 	return (-1);
