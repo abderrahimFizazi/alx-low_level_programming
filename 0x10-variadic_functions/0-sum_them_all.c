2b6e8d86be813f4f61717ed8ecd840f861c650b3
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-10-13 10:35:37 -0700

    test

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - executes a function given as a parameter
+ * on each element of an array
+ * @n: array to iterate over
+ * Return: size of the array
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (!n)
+		return (0);
+	va_list args;
+
+	va_start(args, n);
+
+	int i = 0, sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	return (sum);
+}
