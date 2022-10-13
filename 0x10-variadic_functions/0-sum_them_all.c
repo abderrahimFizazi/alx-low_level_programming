9e9471c644922dd374a957b6c35e6952525889b0
abderrahimFizazi <abderrahim.fazazi@etu.uae.ac.ma>
2022-10-13 10:47:57 -0700

    test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,20 +11,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+        int i = 0, sum = 0;
 
-va_list li;
-int sum = 0;
-unsigned int i;
+	va_list args;
 
-va_start(li, n);
-
-if (n != 0)
-{
-for (i = 0; i < n; i++)
-sum += va_arg(li, int);
-}
-
-va_end(li);
-return (sum);
-
+	va_start(args, n);
+	if (n != 0)
+	{
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	}
+	va_end(args);
+	return (sum);
 }
