26eb54757189ac662fd00ff8ceec633b6884e5be
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-11-26 10:12:20 -0800

    hey

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-  int fd, t, s = 0;
+int fd, t, s = 0;
 
-  if (!filename)
-    return (-1);
+if (!filename)
+return (-1);
 
-  fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-  if (fd < 0)
-    return (-1);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd < 0)
+return (-1);
 
-  if (text_content)
-    {
-      while (text_content[s])
-	s++;
-      t = write(fd, text_content, s);
-      if (t != s)
-	return (-1);
-    }
+if (text_content)
+{
+while (text_content[s])
+s++;
+t = write(fd, text_content, s);
+if (t != s)
+return (-1);
+}
 
-  close(fd);
+close(fd);
 
-  return (1);
+return (1);
 }
